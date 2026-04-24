11ffe87b10ba4c97de46c9830e98ac1bfd5e9dc9
daniellopezgamer4-eng <daniellopezgamer4@gmail.com>
2026-04-24 14:55:54 -0400

    ultimos ejercicios

--- for.py
+++ for.py
@@ -5,15 +5,22 @@
 # for i in range(1, num+1):
 #     print(f"{num} x {i} = {num*i}")
 
-numN=int(input("Ingrese cantidad de notas")) 
-for i in range(1,numN+1):
-    nota=float(input("Ingrese su nota"))
-    if numN>1:
-        prom=float(nota+i/numN)
-        print("su promedio es :",round(prom))
-if prom<4.0:
-    print("Estudiante reprobo")
-else:
-    print("Estudiante aprobo")
+# numN=int(input("Ingrese cantidad de notas")) 
+# for i in range(1,numN+1):
+#     nota=float(input("Ingrese su nota"))
+#     if numN>1:
+#         prom=float(nota*i/numN)
+        
+# if prom<4.0:
+#     print("su promedio es :", prom)
+#     print("Estudiante reprobo")
+# else:
+#     print("su promedio es :", prom)
+#     print("Estudiante aprobo")
 
 
+num=int(input("Ingrese un numero"))
+total=0
+for i in range(num):
+    total+=i+1 
+    print(f"el resultado total es {total}" )
