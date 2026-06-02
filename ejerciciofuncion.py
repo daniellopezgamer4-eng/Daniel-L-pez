163bb35ccc382143b7f32c823a3129b41d4232fa
daniellopezgamer4-eng <daniellopezgamer4@gmail.com>
2026-06-02 10:35:30 -0400

    Add files via upload

---
+++ ejerciciofuncion.py
@@ -0,0 +1,12 @@
+numeros=[1,2,3,4,5,6,7,8,9,10]
+suma=0
+for i in numeros :
+    print(i)
+    if i %2==0:
+        print("es par")
+        suma += i
+
+
+print("la suma de los numeros que son pares " , suma) 
+
+        
