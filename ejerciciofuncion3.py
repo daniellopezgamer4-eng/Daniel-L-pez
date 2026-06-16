946765a7c4194002905b378d0d84ebf7c2794869
daniellopezgamer4-eng <daniellopezgamer4@gmail.com>
2026-06-16 09:49:51 -0400

    Refactor game management functions and inputs

--- ejerciciofuncion3.py
+++ ejerciciofuncion3.py
@@ -1,61 +1,77 @@
 Juegos={
-    "Hackandslash":"Devil may cry 3 ",
-    "run and gun": "Cuphead" ,
-    "Plataformer" : "Super mario galaxy" , 
-    "Rpg": "Final fantasy 7"
+  "Hackandslash":"Devil may cry 3 ",
+  "run and gun": "Cuphead" ,
+  "Plataformer" : "Super mario galaxy" ,
+  "Rpg": "Final fantasy 7"
 }
-
+ 
+ 
+ 
+ 
 def agregar():
-    Juegos["Sandbox"]="Minecraft"
-
+  genero = input("Genero que desea añadir? :")
+  juego = input("juego que sea del genero añadido : ")
+  Juegos[genero]= juego
+ 
+ 
+ 
+ 
 def borrar():
-    eliminar=input("Que desea eliminar?")
-    if eliminar in Juegos.keys() :
-        del Juegos[eliminar]
-
+  eliminar=input("Indique genero que desea eliminar: ")
+  if eliminar in Juegos.keys() :
+    del Juegos[eliminar]
+ 
+ 
+ 
 def actualizar():
-    actualizar=input("Que genero desea actualizar?") 
-    if actualizar in Juegos.values() :
-        Juegos[actualizar] 
-         
-
+  genero=input("¿Que genero desea actualizar?: ")
+  juego = input("Nuevo nombre del juego : ")
+  if genero in Juegos :
+    Juegos[genero]=juego
+    print('entrando')
+ 
+ 
 def mostrar() :
-    print(Juegos)
-
+  print()
+  print('Lista de juegos:')
+  print('----------------')
+  for key, value in Juegos.items():
+    print(key,' - ', value)
+  print()
+ 
+ 
+ 
 while True:
-    print("1.- Agregar dato")
-    print("2.- Borrar datos")
-    print("3.-Actualizar")
-    print("4.-Mostrar datos")
-    print( "5.-Salir") 
-    op=int(input("Elija una opcion"))
-    for key, value in Juegos.items():
-        print(key, value)
-    if op == 1 : 
-        agregar()
-    elif op == 2 :
-        borrar()
-    elif op == 3 :
-        actualizar()
-    elif op == 4 :
-       mostrar()
-    elif op == 5 :
-        break
-        
-
-    
-# print(Juegos["Hackandslash"])
-
-     
-# del Juegos ["Plataformer"] 
-
-     
-# Juegos["Rpg"]="Final Fantasy Remake Integrade" 
-
-# Juegos["Sandbox"]="Minecraft" 
-
-# print(Juegos)
-        
-    
-     
-
+  print()
+  print("1.- Agregar dato ")
+  print("2.- Borrar datos ")
+  print("3.- Actualizar ")
+  print("4.- Mostrar datos ")
+  print("5.- Salir")
+  print()
+ 
+ 
+  op=int(input("Elija una opcion: "))
+ 
+ 
+ 
+ 
+  if op == 1 :
+    agregar()
+ 
+ 
+  elif op == 2 :
+    borrar()
+ 
+ 
+  elif op == 3 :
+    actualizar()
+ 
+ 
+  elif op == 4 :
+    mostrar()
+ 
+ 
+  elif op == 5 :
+    break
+ 
