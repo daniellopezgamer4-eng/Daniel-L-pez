c96a190296936462cc9a0473559c7b6ce934f7c4
daniellopezgamer4-eng <daniellopezgamer4@gmail.com>
2026-07-03 14:20:04 -0400

    Refactor Diccionariodoble.py with new features

--- Diccionariodoble.py
+++ Diccionariodoble.py
@@ -19,6 +19,10 @@ operaciones={
 
 }
 
+def muestraautos(dic) : 
+    for id , vehiculo in dic.items() :
+        print(f"{id}.-{vehiculo}")
+
 
 def autos_vendidos_por_marca(marca) :
     total = 0 
@@ -34,9 +38,9 @@ def autos_vendidos_por_marca(marca) :
     print(f"el numero total de autos vendidos de la marca {marca.upper()} es {total} ")
 
 
-# 
-# marca=input("Ingrese la marca a buscar : ")
-# autos_vendidos_por_marca(marca) 
+
+marca=input("Ingrese la marca a buscar : ")
+autos_vendidos_por_marca(marca) 
 
 def busqueda_por_año(año_min,año_max) :
     elementos=[]
@@ -63,3 +67,115 @@ while True :
         break
     except : 
         print("Los datos ingresados deben ser numeros enteros")
+print(operaciones)
+id_auto=input("Ingrese el id del auto que desea actualizar :")
+
+def actualizar_fecha_venta(id_auto) : 
+    if id_auto in operaciones :
+        nueva_fecha=input("Ingrese la nueva fecha del auto : ")
+        operaciones[id_auto][-1]=nueva_fecha
+        return True
+    else :
+        print("El id no se encuentra ")
+        return False
+
+
+while True : 
+    id=input("Ingrese el valor del ID a actualizar :")
+    fecha=input("Ingrese la fecha a actualizar :")
+
+    if actualizar_fecha_venta(id,fecha) :
+        print("Actualizado correctamente")
+    else : 
+        print("El id no se encuentra ")
+    next=input("¿Desea actualizar otro vehiculo?(s/n)")
+    if next.lower() == "n" : 
+        break
+
+
+def validaAÑO(a):
+    if a<1900 :
+        return True
+    else:
+        return False
+
+def validaRanking(a):
+    if a < 1 and a >6 :
+        return True
+    else:
+        return False
+
+
+def validaString(s) :
+    if s == "" or s == " " :
+        return True
+    else : 
+        return False
+
+
+
+
+def creauto() : 
+    id=input("Ingrese el ID:")
+    if validaString(id) : 
+        print("Dato incorrecto")
+        return
+    marca=input("Ingrese la marca : ")
+    if validaString(marca) : 
+        print("Dato incorrecto")
+        return
+    modelo=input("Ingrese el modelo : ")
+    if validaString(modelo) : 
+        print("Dato incorrecto")
+        return
+    año=int(input("Ingrese el año : "))
+    if validaAÑO(año) : 
+        print("El año debe ser superior a 1900")
+        return
+    ranking=int(input("Ingrese el ranking : "))
+    if validaRanking(ranking) :
+        print("El ranking tiene que ser un numero entero entre 1 y 5")
+        return
+    fecha=input("Ingrese la fecha ")
+    autos[id]=[marca,modelo,año,ranking]
+    operaciones[id]=[fecha,"Pendiente"]
+
+
+
+
+
+def eliminar_auto(id_auto) :
+    if id_auto in autos :
+       del autos[id_auto]
+       del operaciones[id_auto]
+       return True
+    else : 
+        return False
+
+
+
+
+# Menu principal 
+while True :
+    print("=====Menu Principal=====") 
+    print("1.-Autos vendidos por marca")
+    print("2.-Busqueda por año ")
+    print("3.-Actualizar fecha de venta")
+    print("4.-Añadir auto")
+    print("5.-Eliminar auto")
+    print("6.-Salir")
+
+    op=int(input("Elija una opcion "))
+    if op == 1 : 
+        autos_vendidos_por_marca() 
+    elif op == 2 : 
+        busqueda_por_año()
+    elif op == 3 : 
+        actualizar_fecha_venta()
+    elif op == 4 : 
+        creauto()
+    elif op == 5 : 
+        eliminar_auto() 
+    elif op == 6 : 
+        break
+        
